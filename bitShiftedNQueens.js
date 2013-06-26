cc65a6f027a92ebc04b81744f8e2781ed26eda15
Hack Reactor Students <github@hackreactor.com>
2013-06-26 16:37:04 -0700

    laying down bitshift logic

--- bitShiftedNQueens.js
+++ bitShiftedNQueens.js
@@ -10,18 +10,49 @@ window.bitShiftedNQueens = function(n){
   for (var r = 0; r < n; r++){
     // col
     for (var c = 0; c < n; c++){
-      // if on diag . . . . . bin -> dec = 2^(array position)
-      if (c === r) {
+      // check +diag conflict: output >> #*3
+      //       -diag conflict: output >> #*5
+      //       horiz conflict: output >> #*1
+      //        vert conflict: output >> #*4
+      // vert > horiz > -diag > +diag
+      if (output & output >> n || output & output >> ((2*n) - 1) || output & output >> ((3*n) - 1)) {
+        // COLLISION!
+      } else if (output & output >> c-1 || output & output >> c-2 || output & output >> c-3){
+        // COLLISION!
+      } /*else if (output & output >> (4*(r-1)+(c-1)) || output & output >> (4*(r-2)+(c-2)) || output & output >> (4*(r-3)+(c-3))){
+        // COLLISION!
+      } else if (output & output >> (4*(r-1)-(c-1)) || output & output >> (4*(r-2)-(c-2)) || output & output >> (4*(r-3)-(c-3))){
+        // COLLISION!
+      } */else {
+        // assign bin val of cur pos
+        // bin -> dec = 2^(array position)
         temp = Math.pow(2, (4 * r) + c);
         output += temp;
         demo[r][c] = temp;
-      } else {
+      }
+      if (!temp) {
         demo[r][c] = 'x';
       }
+      temp = 0;
     }
   }
   console.log(output, "demo!\n", _.flatten(demo));
 };
+
+// window.depthSearch = function(){
+//   var rootNode = structure.getRoot();
+//   var preOrder = new Array();
+//   var postOrder = new Array();
+//   function DepthFirst( rootNode ){
+//     preOrder[ preOrder.length ] = rootNode;
+
+//     for( var child in rootNode ){
+//       DepthFirst( child );
+//     }
+
+//     postOrder[ postOrder.length ] = rootNode;
+//   }
+// };
 
 var factorial = function(n){
   if (n === 0 || n === 1) { return 1; }
