bf494359333de9ce3cc4b1c9ffee849df72de79a
Hack Reactor Students <github@hackreactor.com>
2013-06-27 16:49:43 -0700

    implementation of recursion, removal of 2-dim 'flag' array

--- bitShiftedNQueens.js
+++ bitShiftedNQueens.js
@@ -1,48 +1,52 @@
 window.bitShiftedNQueens = function(n){
   var curTotal = 0;
   var flag = [], r = 1;
-  var column = 0, backward = 0, forward = 0, qArray = [], solutions=0;
+  var column = 0, backward = 0, forward = 0, qArray = [], solutions = 0;
   // set up collision space
   for (var x = 0; x < n; x++){
-    flag.push([]);
+    //flag.push([]);
     qArray[x]=0;
   }
   //debugger;
-  var rowSpace = function(r){
-    //debugger;
-    if (r<1){r=1;}
-    for (r; r <= n; r++){
-      // if not first row, shift \ & /
-      for (var c = 0; c < n; c++){
-        // if col, \, / are all not blacklisted
-        if (!(_.contains(flag[r], c)) && !(column & Math.pow(2, c)) && !(backward & Math.pow(2, c)) && !(forward & Math.pow(2, c))){
-          qArray[c] = r;
-          column += Math.pow(2,c); // add column
-          backward += Math.pow(2,c);
-          forward += Math.pow(2,c);
-          console.log("R:",r,"\tF:",flag,"\tQ:",qArray,"\tC:",column,"\tB:",backward,"\tF:",forward);
-          break;
+  var rowSpace = function(r, backward, forward, column){
+  //debugger;
+    // if not first row, shift \ & /
+    for (var c = 1; c <= n; c++){
+      // if col, \, / are all not blacklisted
+      var binIndex = Math.pow(2, c);
+      if (!(column & binIndex) && !(backward & binIndex) && !(forward & binIndex)){
+        qArray[r] = c;
+        column += binIndex; // add column
+        backward += binIndex;
+        forward += binIndex;
+        console.log("R:",r,"\tQ:",qArray,"\tC:",column,"\tB:",backward,"\tF:",forward);
+        r++;
+        if (_.contains(qArray, 0)){
+          var newBackward = (backward >> 1);
+          var newForward = (forward << 1);
+          var newColumn = column;
+          rowSpace(r, newBackward, newForward, newColumn);
+        } else {
+          solutions++;
         }
+        // if row was assigned, shift bits
+        // if (_.contains(qArray, r)){
+        //   backward >>= 1;
+        //   forward <<= 1;
+        // // else if row not assigned, swap
+        // } else if (r > 1){
+          //console.log("R:",r,"\tF:",flag);
+        qArray[r] = 0;
+        r--;
+        //backward <<= 1;
+        //forward >>= 1;
+        backward -= binIndex; // unshift
+        forward -= binIndex; // unshift
+        column -= binIndex;
+        //rowSpace(r);
       }
-      // if row was assigned, shift bits
-      if (_.contains(qArray, r)){
-        backward >>= 1;
-        forward <<= 1;
-        //flag = -1;
-      // else if row not assigned, swap
-      } else if (r > 1){
-        console.log("R:",r,"\tF:",flag);
-        flag[r] = []; // destroy broken descendants
-        r--; // reduce row
-        var temp = _.indexOf(qArray, r);
-        flag[[r][temp]] = temp; // find index of queen in previous row
-        qArray[ temp ] = 0; // reset queen
-        backward <<= 1; // unshift
-        forward >>= 1; // unshift
-      }
-      //flag = [];
     }
-    console.log("queen", qArray, "\nbackward", backward, "\nforward", forward);
+    //console.log("solutions", solutions,"\tqueen", qArray, "\tback", backward, "\tfor", forward);
   };
   rowSpace(r);
 };
