0820ae812e9ee68d47c5b9682883173e2d6ff6d8
Hack Reactor Students <github@hackreactor.com>
2013-06-27 18:28:16 -0700

    SUCCESS!

--- bitShiftedNQueens.js
+++ bitShiftedNQueens.js
@@ -1,55 +1,22 @@
 window.bitShiftedNQueens = function(n){
-  var curTotal = 0;
-  var flag = [], r = 1;
-  var column = 0, backward = 0, forward = 0, qArray = [], solutions = 0;
-  // set up collision space
-  for (var x = 0; x < n; x++){
-    //flag.push([]);
-    qArray[x]=0;
-  }
-  //debugger;
-  var rowSpace = function(r, backward, forward, column){
-  //debugger;
-    // if not first row, shift \ & /
-    for (var c = 1; c <= n; c++){
-      // if col, \, / are all not blacklisted
-      var binIndex = Math.pow(2, c);
-      if (!(column & binIndex) && !(backward & binIndex) && !(forward & binIndex)){
-        qArray[r] = c;
-        column += binIndex; // add column
-        backward += binIndex;
-        forward += binIndex;
-        console.log("R:",r,"\tQ:",qArray,"\tC:",column,"\tB:",backward,"\tF:",forward);
-        r++;
-        if (_.contains(qArray, 0)){
-          var newBackward = (backward >> 1);
-          var newForward = (forward << 1);
-          var newColumn = column;
-          rowSpace(r, newBackward, newForward, newColumn);
+  var solutions = 0;
+  var rowSpace = function(r, backward, column, forward){
+    for (var c = 0; c < n; c++){
+      var placement = Math.pow(2,c);
+      var conflicts = (column | backward | forward);
+      if (!(conflicts & placement)) {
+        if (r+1 < n){
+          rowSpace(r+1, (backward + placement) >> 1, column + placement, (forward + placement) << 1);
         } else {
           solutions++;
         }
-        // if row was assigned, shift bits
-        // if (_.contains(qArray, r)){
-        //   backward >>= 1;
-        //   forward <<= 1;
-        // // else if row not assigned, swap
-        // } else if (r > 1){
-          //console.log("R:",r,"\tF:",flag);
-        qArray[r] = 0;
-        r--;
-        //backward <<= 1;
-        //forward >>= 1;
-        backward -= binIndex; // unshift
-        forward -= binIndex; // unshift
-        column -= binIndex;
-        //rowSpace(r);
       }
     }
-    //console.log("solutions", solutions,"\tqueen", qArray, "\tback", backward, "\tfor", forward);
   };
-  rowSpace(r);
+  rowSpace(0, 0, 0, 0);
+  console.log(solutions);
 };
+
 
 
 // // window.depthSearch = function(){
@@ -73,5 +40,3 @@ window.bitShiftedNQueens = function(n){
 //     return factorial(n-1) * n;
 //   }
 // };
-
-bitShiftedNQueens(4);
